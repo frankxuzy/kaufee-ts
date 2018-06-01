b08ebf20db881c1d4622b5d677dbf68f7aa2ea77
Frank <xuzy213@gmail.com>
2018-06-01 22:15:14 +0800

    add tables and seeds

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const express = require('express')
 
+const authRoute = require('./routes/auth')
+
 const server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
+server.use('api/v1/auth', authRoute)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
